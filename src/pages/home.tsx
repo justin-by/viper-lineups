88ff82fe14c7dc6b8289a02c46391c49c0d8808b
justin-by <flaminlinkvp@gmail.com>
2021-09-08 15:40:22 -0700

    navbar setup

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,7 +12,7 @@ const HomePage: React.FunctionComponent<IPage> = props => {
                 <li className='nav-logo'>
                     <a href='/'>
                         <img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAMAAAAp4XiDAAAATlBMVEUAAAD+/v7+/v7+/v7+/v7+/v7////+/v7+/v7+/v7////////+/v7+/v7+/v7+/v7+/v7+/v7+/v7////7+/v+/v7+/v7+/v7+/v7/////XD+bAAAAGXRSTlMA3Pw4SnVF8+3QEAqEK7WpUiGmIlMst1Qb94EzkwAAALZJREFUSMftkkE
-                        SgjAQBANKRI1BVNT9/0f1NgnD7t6p9JGqJjWdhEbjkILLEGulf3lG7I+1IqebbcyjrBXpLpZxPQsrki3lIVuKJGO6bCtIwNMVBQl4uqIgAU3XFcnKdEORpEw3lP7O0x0FCTDdUJCAp7OyCMj1dBBDxSQgldPBW2mPBJiu3dlzFEowl9+WlUB/pJM/ymutEwwdGP5Vv+xwAsZNwHgJGD8B4ydg9p+gcKbQaJT8AL5WJzJ+xEFZAAAAAElFTkSuQmCC' alt='val logo'/>
+                        SgjAQBANKRI1BVNT9/0f1NgnD7t6p9JGqJjWdhEbjkILLEGulf3lG7I+1IqebbcyjrBXpLpZxPQsrki3lIVuKJGO6bCtIwNMVBQl4uqIgAU3XFcnKdEORpEw3lP7O0x0FCTDdUJCAp7OyCMj1dBBDxSQgldPBW2mPBJiu3dlzFEowl9+WlUB/pJM/ymutEwwdGP5Vv+xwAsZNwHgJGD8B4ydg9p+gcKbQaJT8AL5WJzJ+xEFZAAAAAElFTkSuQmCC' alt='val logo' />
                     </a>
                 </li>
                 <li>
@@ -20,9 +20,14 @@ const HomePage: React.FunctionComponent<IPage> = props => {
                         <button className='nav-dropdown-button'>Maps</button>
                     </div>
                 </li>
+                <a className='nav-about' href='/about'>
+                    <div className='nav-about-text'>
+                        About
+                    </div>
+                </a>
             </ul>
         </div>
-        )
+    )
 }
 
 export default HomePage;
