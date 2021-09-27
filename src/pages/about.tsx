5e49449c40f90affc0bc1311e562daf3784593ed
justin-by <flaminlinkvp@gmail.com>
2021-09-26 20:43:45 -0700

    about page added, navbar

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import NavBar from './navbar';
 import IPage from '../interfaces/page';
 
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 
 const AboutPage: React.FunctionComponent<IPage & RouteComponentProps<any>> = props => {
-    // const [message, setMessage] = useState<string>('');
+    
 
 
     return (
-        <div>
-            <p>HOME!</p>
-            <Link to="/">Go to the home page!</Link>
-        </div>
+        <>
+            <NavBar name={'NavBar'} />
+        </>
     );
 }
 
