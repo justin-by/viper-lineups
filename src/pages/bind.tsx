4e8e652afa2837dd49af2adfc44d12363c73f895
justin-by <flaminlinkvp@gmail.com>
2021-09-29 13:59:30 -0700

    viper lineup body, video, and side image setup

--- src/pages/bind.tsx
+++ src/pages/bind.tsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player'
 import NavBar from './navbar';
 import IPage from '../interfaces/page';
+
+import astairstand from '../images/iceboxlineups/astairstand.png'
 
 import './bind.css'
 
 
 const BindPage: React.FunctionComponent<IPage> = props => {
+
+    
 
 
 
@@ -27,22 +32,42 @@ const BindPage: React.FunctionComponent<IPage> = props => {
                     </div>
                 </div>
                 <div className='lineups-wrapper'>
-                    
+
                     <div className='lineups-center-map-header'>
 
                         <div className='lineups-header-text-wrapper'>
                             <div className='lineups-map-title'>
                                 <p id='map-title-text'>ICEBOX</p>
-                                <p id='map-location'>TEMP</p>
+                                <p id='map-location'>RUSSIA</p>
                             </div>
                         </div>
-
                     </div>
+
                     <div className='lineups-center-container'>
+                        <div className='lineups-center-wrapper'>
+                            <div className='lineups-center-header'>
+                                <h2 id='lineups-center-header-text'>A SITE FROM RAMP</h2>
+                            </div>
+                            <div className='lineups-center-body'>
+                                <p id='lineups-center-body-text'>
+                                    This lineup allows Viper to safely play for time by hiding behind A Ramp.
+                                    This lineup allows Viper to safely play for time by hiding behind A Ramp.
+                                    This lineup allows Viper to safely play for time by hiding behind A Ramp.
+                                </p>
+                                <div className='lineup-video-container'>
+                                    <ReactPlayer url='https://youtu.be/YmT5g1QgVns' playing={true} loop={true} muted={true} controls={true} className='lineup-video' />
+                                </div>
 
+                            </div>
+
+                        </div>
                     </div>
-                    <div className='lineups-right-container'>
 
+                    <div className='lineups-right-container'>
+                        <div className='lineup-step-image' style={{
+                            backgroundImage: `url(${astairstand})`
+                        }} />
+                        <div className='lineup-step-image' />
                     </div>
                 </div>
 
