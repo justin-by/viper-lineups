38fbf09b20cfb3dbffbeeaa1631b68c004814cc8
justin-by <flaminlinkvp@gmail.com>
2021-09-29 10:08:26 -0700

    lineup select tabs and map header

--- src/pages/bind.tsx
+++ src/pages/bind.tsx
@@ -15,12 +15,27 @@ const BindPage: React.FunctionComponent<IPage> = props => {
 
             <div className='lineups-background'>
                 <div className='lineups-left-container'>
-                    <div>
-                        
+                    <div className='lineup-select'>
+                        <div className='lineup-select-text-wrapper'>
+                            <div className='lineup-select-title'>
+                                A SITE FROM RAMP
+                            </div>
+                            <div className='lineup-select-desc'>
+                                This lineup allows Viper to safely play for time by hiding behind A Ramp.
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className='lineups-wrapper'>
+                    
                     <div className='lineups-center-map-header'>
+
+                        <div className='lineups-header-text-wrapper'>
+                            <div className='lineups-map-title'>
+                                <p id='map-title-text'>ICEBOX</p>
+                                <p id='map-location'>TEMP</p>
+                            </div>
+                        </div>
 
                     </div>
                     <div className='lineups-center-container'>
