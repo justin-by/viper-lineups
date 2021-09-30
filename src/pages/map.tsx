82cd6322601244385db12837af2790a16530da8f
justin-by <flaminlinkvp@gmail.com>
2021-09-30 15:00:48 -0700

    bind page complete

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -25,7 +25,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 2,
                 title: 'A SITE FROM MID',
-                desc: 'Requires you to crouch! This lineup allows Viper to safely play for time from mid. One of the biggest strengths of this lineup is being extremely close to the bomb sight while also being away from the expected, more standard lineup spots.',
+                desc: 'Requires you to crouch! This lineup allows Viper to safely play for time from mid. One of the biggest strengths of this lineup is being extremely close to the spike while also being away from the expected, more standard lineup spots.',
                 shortDesc: 'This lineup allows Viper to play for lineups by sitting besides boiler. This position allows Viper to quickly re-enter site.',
                 video: 'IZNky8EF1xA',
                 standImage: 'https://i.imgur.com/JmFBuO1.png',
@@ -34,7 +34,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 3,
                 title: 'A SITE FROM BELT',
-                desc: 'This lineup is very safe as it allows Viper to sit behind belt. Although it will be hard for enemies to run all the way to viper, it is equally long for Viper to re-enter site. However if the bomb is positioned correctly Viper can hold from ontop of belt.',
+                desc: 'This lineup is very safe as it allows Viper to sit behind belt. Although it will be hard for enemies to run all the way to viper, it is equally long for Viper to re-enter site. However if the spike is positioned correctly Viper can hold from ontop of belt.',
                 shortDesc: 'This lineup is very safe as it completely provides Viper cover.',
                 video: 'kR6ukvC7eJA',
                 standImage: 'https://i.imgur.com/q9MyU25.png',
@@ -61,22 +61,73 @@ const MapPage: React.FunctionComponent<IPage> = props => {
         ]
     }
 
-    
-
-    const [mapInfo, setMapInfo] = useState(icebox);
-    const [currentLineup, setCurrentLineup] = useState(mapInfo.lineups[0])
-
-    if (window.location.href === '/icebox') {
-        setMapInfo(icebox)
+    const bind = {
+        map: 'BIND',
+        region: 'MOROCCO',
+        mapImage: 'https://i.imgur.com/yS2MMVM.png',
+        lineups: [
+            {
+                id: 1,
+                title: 'A SITE FROM SHORT',
+                desc: 'This lineup is used from a common spot but provides decent protection from anyone pushing. Tucking into the cubby buys Viper a couple of extra seconds for her to use her lineups. It is worth noting that because the lineup is used so closely, it takes around 4 or more seconds for the poison to land.',
+                shortDesc: 'This easy lineup allows Viper to control site while being super nearby.',
+                video: 'gin4ZgrZmJM',
+                standImage: 'https://i.imgur.com/hkxAjmG.png',
+                aimImage: 'https://i.imgur.com/ZGjqBdw.png'
+            },
+            {
+                id: 2,
+                title: 'A SITE FROM SHOWER',
+                desc: 'This lineup is great for being unexpected and is perfect for being a rotation lineup. This can be used immediately after the A Short lineup, preventing Viper from being caught.',
+                shortDesc: 'This unexpected lineup is used outside of A Showers.',
+                video: 'LZl2J_mWhVU',
+                standImage: 'https://i.imgur.com/ecFAlng.png',
+                aimImage: 'https://i.imgur.com/1YlHykI.png'
+            },
+            {
+                id: 3,
+                title: 'A SITE FROM SPAWN',
+                desc: 'Although this lineup is rarely used, it can prove to be very helpful in some situations. This lineup can easily be used if Viper holds defender spawn or is flanking from B site.',
+                shortDesc: "This tricky lineup is used from the enemy's spawnpoint.",
+                video: '8nBW7qW4ai0',
+                standImage: 'https://i.imgur.com/XfLrWGy.png',
+                aimImage: 'https://i.imgur.com/NxJmyUK.png'
+            },
+            {
+                id: 4,
+                title: 'B SITE FROM HOOKAH',
+                desc: "This is a standard lineup that's very useful for being close to the spike. Viper is able to quickly move into Hookah and easily peek the planted spike. Because this spot is so close to the site, expect that enemies may quickly push you.",
+                shortDesc: "This is a standard lineup that's very useful for being close to the spike.",
+                video: 'E6vNzK_k8PQ',
+                standImage: 'https://i.imgur.com/Fzhx9Ok.png',
+                aimImage: 'https://i.imgur.com/WSbcVki.png'
+            },
+            {
+                id: 5,
+                title: 'B SITE FROM LONG',
+                desc: 'This lineup has the perfect amount of distance to protect Viper and allow her to peek the spike.',
+                shortDesc: 'This easy lineup allows Viper to control site while being nearby.',
+                video: 'DLTN_Wv1tLk',
+                standImage: 'https://i.imgur.com/HGHbfYN.png',
+                aimImage: 'https://i.imgur.com/DsSOicG.png'
+            },
+        ]
     }
 
 
 
 
 
+    const [mapInfo, setMapInfo] = useState(bind);
+    const [currentLineup, setCurrentLineup] = useState(mapInfo.lineups[0])
 
-
-
+    useEffect(() => {
+        if (window.location.pathname === '/icebox') {
+            setMapInfo(icebox)
+        } else if (window.location.pathname === '/bind') {
+            setMapInfo(bind)
+        }
+    }, [mapInfo])
 
     return (
         <>
