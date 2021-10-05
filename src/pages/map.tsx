2662295d6f64cba759c97f0997a94a35e9b2a54e
justin-by <flaminlinkvp@gmail.com>
2021-10-05 14:37:53 -0700

    haven page complete

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -5,6 +5,7 @@ import IPage from '../interfaces/page';
 
 
 import './map.css'
+import { SIGTERM } from 'constants';
 
 
 const MapPage: React.FunctionComponent<IPage> = props => {
@@ -121,48 +122,83 @@ const MapPage: React.FunctionComponent<IPage> = props => {
         lineups: [
             {
                 id: 1,
-                title: 'A SITE FROM STAIRS',
-                desc: 'This lineup allows Viper to safely play for time by hiding behind A Ramp. This lineup provides great protection and holds an element of surprise. Viper can quickly re-enter site through either stairs or screen.',
-                shortDesc: 'This lineup allows Viper to play for lineups by sitting besides A Stairs. This position allows Viper to quickly re-enter site.',
-                video: 'YmT5g1QgVns',
-                standImage: 'https://i.imgur.com/nfFxmKP.png',
-                aimImage: 'https://i.imgur.com/MSFhVE0.png'
+                title: 'A SITE FROM LONG',
+                desc: 'Because of this cubby in A Long, Viper is given protection from anyone pushing from site. The only angle Viper has to worry about is flank and A Short. Stand ontop of the sandbags in the first picture and aim on the first column in the second picture.',
+                shortDesc: 'Standard lineup in which Viper is able to buy time from A Long.',
+                video: 'Tei6dmbtJMI',
+                standImage: 'https://i.imgur.com/5QVrANl.png',
+                aimImage: 'https://i.imgur.com/E9202s4.png'
             },
             {
                 id: 2,
-                title: 'A SITE FROM MID',
-                desc: 'Requires you to crouch! This lineup allows Viper to safely play for time from mid. One of the biggest strengths of this lineup is being extremely close to the spike while also being away from the expected, more standard lineup spots.',
-                shortDesc: 'This lineup allows Viper to play for lineups by sitting besides boiler. This position allows Viper to quickly re-enter site.',
-                video: 'IZNky8EF1xA',
-                standImage: 'https://i.imgur.com/JmFBuO1.png',
-                aimImage: 'https://i.imgur.com/U3Qr6GZ.png'
+                title: 'B SITE FROM SPAWN',
+                desc: "This lineup is the most protective as it's used far away from the bomb site. After shooting both snake bites, run to mid window and hold bomb. Run into the corner with the pot in attacker spawn and shoot at the top of the golden bell. ",
+                shortDesc: 'Although rarely used, this lineup can be used if spike is planted in the center of B Site.',
+                video: 'quISQK3Yg3U',
+                standImage: 'https://i.imgur.com/3fSDRGd.png',
+                aimImage: 'https://i.imgur.com/BA31UjZ.png'
             },
             {
                 id: 3,
-                title: 'A SITE FROM BELT',
-                desc: 'This lineup is very safe as it allows Viper to sit behind belt. Although it will be hard for enemies to run all the way to viper, it is equally long for Viper to re-enter site. However if the spike is positioned correctly Viper can hold from ontop of belt.',
-                shortDesc: 'This lineup is very safe as it completely provides Viper cover.',
-                video: 'kR6ukvC7eJA',
-                standImage: 'https://i.imgur.com/q9MyU25.png',
-                aimImage: 'https://i.imgur.com/tfrZMO6.png'
+                title: 'C SITE FROM LONG',
+                desc: 'Many Viper lineups are used in C Long while this lineup provides a bit more protection by positioning outside of C Long. This also allows Viper to rotate to garage after shooting both Snike Bites for retake. Run to the item on the floor, against the wall as seen in the first picture. Then align the left side of the center line under the gas meter with the tip of the branch.',
+                shortDesc: 'This lineup is used to buy time from outside C Long.',
+                video: 'QGyGl9CXwY0',
+                standImage: 'https://i.imgur.com/4niT5e6.png',
+                aimImage: 'https://i.imgur.com/nePtP3W.png'
             },
             {
                 id: 4,
-                title: 'B SITE FROM CUBBY',
-                desc: 'This is the standard and popular lineup for B site. This spot provides a decent amount of protection from enemies trying to stop Viper but may still be vulernable to rushes.',
-                shortDesc: 'This lineup is the standard lineup for B site.',
-                video: 'iKjy5y19B4U',
-                standImage: 'https://i.imgur.com/HmAaNuv.png',
-                aimImage: 'https://i.imgur.com/KXtv8ax.png'
+                title: 'C SITE FROM MID',
+                desc: 'The strength of this lineup is the ability for Viper to play post plant away from the expected and popular spots. Enemies will usually expect Viper to be positioned near C Long, but Viper can use this spot to avoid being pushed. Sit ontop of the corner of the bricks and align the gas meter bar to form a triangle.',
+                shortDesc: 'Unexpected and unique lineup that will catch people off guard.',
+                video: 'LJodRyQ0UuQ',
+                standImage: 'https://i.imgur.com/YcU9Q7e.png',
+                aimImage: 'https://i.imgur.com/87Avvlq.png'
+            },
+        ]
+    }
+
+    const split = {
+        map: 'SPLIT',
+        region: 'BHUTAN',
+        mapImage: 'https://i.imgur.com/7dBtv1T.png',
+        lineups: [
+            {
+                id: 1,
+                title: 'A SITE FROM LONG',
+                desc: 'Because of this cubby in A Long, Viper is given protection from anyone pushing from site. The only angle Viper has to worry about is flank and A Short. Stand ontop of the sandbags in the first picture and aim on the first column in the second picture.',
+                shortDesc: 'Standard lineup in which Viper is able to buy time from A Long.',
+                video: 'Tei6dmbtJMI',
+                standImage: 'https://i.imgur.com/5QVrANl.png',
+                aimImage: 'https://i.imgur.com/E9202s4.png'
             },
             {
-                id: 5,
-                title: 'B SITE FROM MID',
-                desc: 'Although this lineup seems very difficult, it becomes very easy to remember where to aim after practicing. This lineup requires you to aim at a small star aligned to the top-left of the bigger brighter star. The strength of this lineup is that it is very unexpected and can possible be used during rotation right after the standard B site lineup.',
-                shortDesc: 'This unexpected lineup allows Viper to control site from mid.',
-                video: 'dIQuNvjSmAM',
-                standImage: 'https://i.imgur.com/ElNw4dd.png',
-                aimImage: 'https://i.imgur.com/vNA3glV.png'
+                id: 2,
+                title: 'B SITE FROM SPAWN',
+                desc: "This lineup is the most protective as it's used far away from the bomb site. After shooting both snake bites, run to mid window and hold bomb. Run into the corner with the pot in attacker spawn and shoot at the top of the golden bell. ",
+                shortDesc: 'Although rarely used, this lineup can be used if spike is planted in the center of B Site.',
+                video: 'quISQK3Yg3U',
+                standImage: 'https://i.imgur.com/3fSDRGd.png',
+                aimImage: 'https://i.imgur.com/BA31UjZ.png'
+            },
+            {
+                id: 3,
+                title: 'C SITE FROM LONG',
+                desc: 'Many Viper lineups are used in C Long while this lineup provides a bit more protection by positioning outside of C Long. This also allows Viper to rotate to garage after shooting both Snike Bites for retake. Run to the item on the floor, against the wall as seen in the first picture. Then align the left side of the center line under the gas meter with the tip of the branch.',
+                shortDesc: 'This lineup is used to buy time from outside C Long.',
+                video: 'QGyGl9CXwY0',
+                standImage: 'https://i.imgur.com/4niT5e6.png',
+                aimImage: 'https://i.imgur.com/nePtP3W.png'
+            },
+            {
+                id: 4,
+                title: 'C SITE FROM MID',
+                desc: 'The strength of this lineup is the ability for Viper to play post plant away from the expected and popular spots. Enemies will usually expect Viper to be positioned near C Long, but Viper can use this spot to avoid being pushed. Sit ontop of the corner of the bricks and align the gas meter bar to form a triangle.',
+                shortDesc: 'Unexpected and unique lineup that will catch people off guard.',
+                video: 'LJodRyQ0UuQ',
+                standImage: 'https://i.imgur.com/YcU9Q7e.png',
+                aimImage: 'https://i.imgur.com/87Avvlq.png'
             },
         ]
     }
@@ -179,7 +215,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             setMapInfo(icebox)
         } else if (window.location.pathname === '/bind') {
             setMapInfo(bind)
-        } else if (window.location.pathname === '/bind') {
+        } else if (window.location.pathname === '/haven') {
             setMapInfo(haven)
         }
     }, [mapInfo])
