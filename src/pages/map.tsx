38b46ba3190e769ddc743a0c32aca1e05ba20d74
justin-by <flaminlinkvp@gmail.com>
2021-10-05 15:23:12 -0700

    ascent map complete

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -17,7 +17,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 1,
                 title: 'A SITE FROM STAIRS',
-                desc: 'This lineup allows Viper to safely play for time by hiding behind A Ramp. This lineup provides great protection and holds an element of surprise. Viper can quickly re-enter site through either stairs or screen.',
+                desc: 'Run to the corner of the right lamp and aim at where the stair meets the bottom-left corner of the highlighted triangle. This lineup allows Viper to safely play for time by hiding behind A Ramp. This lineup provides great protection and holds an element of surprise. Viper can quickly re-enter site through either stairs or screen.',
                 shortDesc: 'This lineup allows Viper to play for lineups by sitting besides A Stairs. This position allows Viper to quickly re-enter site.',
                 video: 'YmT5g1QgVns',
                 standImage: 'https://i.imgur.com/nfFxmKP.png',
@@ -26,7 +26,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 2,
                 title: 'A SITE FROM MID',
-                desc: 'Requires you to crouch! This lineup allows Viper to safely play for time from mid. One of the biggest strengths of this lineup is being extremely close to the spike while also being away from the expected, more standard lineup spots.',
+                desc: 'Requires you to crouch! Run to the corner of the box besides boiler and aim at the first open segment of the blue beams. This lineup allows Viper to safely play for time from mid. One of the biggest strengths of this lineup is being extremely close to the spike while also being away from the expected, more standard lineup spots.',
                 shortDesc: 'This lineup allows Viper to play for lineups by sitting besides boiler. This position allows Viper to quickly re-enter site.',
                 video: 'IZNky8EF1xA',
                 standImage: 'https://i.imgur.com/JmFBuO1.png',
@@ -35,7 +35,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 3,
                 title: 'A SITE FROM BELT',
-                desc: 'This lineup is very safe as it allows Viper to sit behind belt. Although it will be hard for enemies to run all the way to viper, it is equally long for Viper to re-enter site. However if the spike is positioned correctly Viper can hold from ontop of belt.',
+                desc: 'Position against the ramp, right in front of the fourth arrow. Aim at the tip of the same fourth arrow.This lineup is very safe as it allows Viper to sit behind belt.',
                 shortDesc: 'This lineup is very safe as it completely provides Viper cover.',
                 video: 'kR6ukvC7eJA',
                 standImage: 'https://i.imgur.com/q9MyU25.png',
@@ -44,7 +44,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 4,
                 title: 'B SITE FROM CUBBY',
-                desc: 'This is the standard and popular lineup for B site. This spot provides a decent amount of protection from enemies trying to stop Viper but may still be vulernable to rushes.',
+                desc: 'Position at the corner where the metal beams and wall meet. Align the left edge of the right short HUD line to the bottom right corner of the green box. This is the standard and popular lineup for B site. This spot provides a decent amount of protection from enemies trying to stop Viper but may still be vulernable to rushes.',
                 shortDesc: 'This lineup is the standard lineup for B site.',
                 video: 'iKjy5y19B4U',
                 standImage: 'https://i.imgur.com/HmAaNuv.png',
@@ -53,7 +53,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 5,
                 title: 'B SITE FROM MID',
-                desc: 'Although this lineup seems very difficult, it becomes very easy to remember where to aim after practicing. This lineup requires you to aim at a small star aligned to the top-left of the bigger brighter star. The strength of this lineup is that it is very unexpected and can possible be used during rotation right after the standard B site lineup.',
+                desc: 'Sit against the center of the blue box at mid. Aim at the very faint, small star that is to the top-left of the bigger star. Although this lineup seems very difficult, it becomes very easy to remember where to aim after practicing.',
                 shortDesc: 'This unexpected lineup allows Viper to control site from mid.',
                 video: 'dIQuNvjSmAM',
                 standImage: 'https://i.imgur.com/ElNw4dd.png',
@@ -70,7 +70,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 1,
                 title: 'A SITE FROM SHORT',
-                desc: 'This lineup is used from a common spot but provides decent protection from anyone pushing. Tucking into the cubby buys Viper a couple of extra seconds for her to use her lineups. It is worth noting that because the lineup is used so closely, it takes around 4 or more seconds for the poison to land.',
+                desc: 'Sit in the brick corner and align the crosshair to the right of the bottom-right edge of the metal roof. This lineup is used from a common spot but provides decent protection from anyone pushing. Tucking into the cubby buys Viper a couple of extra seconds for her to use her lineups.',
                 shortDesc: 'This easy lineup allows Viper to control site while being super nearby.',
                 video: 'gin4ZgrZmJM',
                 standImage: 'https://i.imgur.com/hkxAjmG.png',
@@ -79,7 +79,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 2,
                 title: 'A SITE FROM SHOWER',
-                desc: 'This lineup is great for being unexpected and is perfect for being a rotation lineup. This can be used immediately after the A Short lineup, preventing Viper from being caught.',
+                desc: 'Align yourself at the corner of the smaller wooden box. Position the tip of the poison cloud icon to the bottom of the leaf. This lineup is great for being unexpected and is perfect for being a rotation lineup. This can be used immediately after the A Short lineup, preventing Viper from being caught.',
                 shortDesc: 'This unexpected lineup is used outside of A Showers.',
                 video: 'LZl2J_mWhVU',
                 standImage: 'https://i.imgur.com/ecFAlng.png',
@@ -88,7 +88,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 3,
                 title: 'A SITE FROM SPAWN',
-                desc: 'Although this lineup is rarely used, it can prove to be very helpful in some situations. This lineup can easily be used if Viper holds defender spawn or is flanking from B site.',
+                desc: 'Position at the corner of where the barrels and wall meet. Position the top edge of the last segment line of the gas meter bar to the metal roof corner. Although this lineup is rarely used, it can prove to be very helpful in some situations. This lineup can easily be used if Viper holds defender spawn or is flanking from B site.',
                 shortDesc: "This tricky lineup is used from the enemy's spawnpoint.",
                 video: '8nBW7qW4ai0',
                 standImage: 'https://i.imgur.com/XfLrWGy.png',
@@ -97,7 +97,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 4,
                 title: 'B SITE FROM HOOKAH',
-                desc: "This is a standard lineup that's very useful for being close to the spike. Viper is able to quickly move into Hookah and easily peek the planted spike. Because this spot is so close to the site, expect that enemies may quickly push you.",
+                desc: "Position at the corner of the box and angle the top-right line of the Health HUD to the top corner of the roof. This is a standard lineup that's very useful for being close to the spike. Viper is able to quickly move into Hookah and easily peek the planted spike. Because this spot is so close to the site, expect that enemies may quickly push you.",
                 shortDesc: "This is a standard lineup that's very useful for being close to the spike.",
                 video: 'E6vNzK_k8PQ',
                 standImage: 'https://i.imgur.com/Fzhx9Ok.png',
@@ -106,7 +106,7 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 5,
                 title: 'B SITE FROM LONG',
-                desc: 'This lineup has the perfect amount of distance to protect Viper and allow her to peek the spike.',
+                desc: 'Hug the corner of the fountain as shown in the first picture. Then align the top-right line of the Health HUD to the bottom of the camera. This lineup has the perfect amount of distance to protect Viper and allow her to peek the spike.',
                 shortDesc: 'This easy lineup allows Viper to control site while being nearby.',
                 video: 'DLTN_Wv1tLk',
                 standImage: 'https://i.imgur.com/HGHbfYN.png',
@@ -132,8 +132,8 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 2,
                 title: 'B SITE FROM SPAWN',
-                desc: "This lineup is the most protective as it's used far away from the bomb site. After shooting both snake bites, run to mid window and hold bomb. Run into the corner with the pot in attacker spawn and shoot at the top of the golden bell. ",
-                shortDesc: 'Although rarely used, this lineup can be used if spike is planted in the center of B Site.',
+                desc: "Run into the corner with the pot in attacker spawn and shoot at the top of the golden bell. This lineup is the most protective as it's used far away from the bomb site. After shooting both snake bites, run to mid window and hold the spike.",
+                shortDesc: 'Although rarely used, this lineup can be used if spike is planted in the center of B Site',
                 video: 'quISQK3Yg3U',
                 standImage: 'https://i.imgur.com/3fSDRGd.png',
                 aimImage: 'https://i.imgur.com/BA31UjZ.png'
@@ -141,8 +141,8 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 3,
                 title: 'C SITE FROM LONG',
-                desc: 'Many Viper lineups are used in C Long while this lineup provides a bit more protection by positioning outside of C Long. This also allows Viper to rotate to garage after shooting both Snike Bites for retake. Run to the item on the floor, against the wall as seen in the first picture. Then align the left side of the center line under the gas meter with the tip of the branch.',
-                shortDesc: 'This lineup is used to buy time from outside C Long.',
+                desc: 'Run to the item on the floor, against the wall as seen in the first picture. Then align the left side of the center line under the gas meter with the tip of the branch. Many Viper lineups are used in C Long while this lineup provides a bit more protection by positioning outside of C Long.',
+                shortDesc: 'This lineup is used to buy time from outside C Long',
                 video: 'QGyGl9CXwY0',
                 standImage: 'https://i.imgur.com/4niT5e6.png',
                 aimImage: 'https://i.imgur.com/nePtP3W.png'
@@ -150,8 +150,8 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 4,
                 title: 'C SITE FROM MID',
-                desc: 'The strength of this lineup is the ability for Viper to play post plant away from the expected and popular spots. Enemies will usually expect Viper to be positioned near C Long, but Viper can use this spot to avoid being pushed. Sit ontop of the corner of the bricks and align the gas meter bar to form a triangle.',
-                shortDesc: 'Unexpected and unique lineup that will catch people off guard.',
+                desc: 'Sit ontop of the corner of the bricks and align the gas meter bar to form a triangle. The strength of this lineup is the ability for Viper to play post plant away from the expected and popular spots. Enemies will usually expect Viper to be positioned near C Long, but Viper can use this spot to avoid being pushed. ',
+                shortDesc: 'Unexpected and unique lineup that will catch people off guard',
                 video: 'LJodRyQ0UuQ',
                 standImage: 'https://i.imgur.com/YcU9Q7e.png',
                 aimImage: 'https://i.imgur.com/87Avvlq.png'
@@ -194,11 +194,55 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             {
                 id: 4,
                 title: 'B SITE FROM HEAVEN',
-                desc: "This lineup required control of B Heaven, but is strong especially in 1v1 scenarios. Align yourself at the center of the right metallic beam and aim at the center of the horizontal beam right before it meets the wall.",
+                desc: "Align yourself at the center of the right metallic beam and aim at the center of the horizontal beam right before it meets the wall. This lineup requires control of B Heaven, but is strong especially in 1v1 scenarios.",
                 shortDesc: 'Unexpected lineup used to hold default from B Heaven',
                 video: 'o-dN7E76D-U',
                 standImage: 'https://i.imgur.com/fBT8nqO.png',
                 aimImage: 'https://i.imgur.com/3dCm3Ga.png'
+            },
+        ]
+    }
+
+    const ascent = {
+        map: 'ASCENT',
+        region: 'ITALY',
+        mapImage: 'https://i.imgur.com/G3xyT7A.png',
+        lineups: [
+            {
+                id: 1,
+                title: 'A SITE FROM MAIN',
+                desc: 'Align yourself against the center of the bycicle. Aim at where the tip of the flag pole and roof column meet. This lineup provides decent protection as enemies have to push main to stop Viper. ',
+                shortDesc: 'This standard lineup is used to cover A Site Default',
+                video: 'W9RuuYmlYH4',
+                standImage: 'https://i.imgur.com/QG6pSBO.png',
+                aimImage: 'https://i.imgur.com/auOBhk2.png'
+            },
+            {
+                id: 2,
+                title: 'A SITE FROM CAT',
+                desc: "Position yourself at the corner of cat and angle the top-right corner of the gas meter bar with the top-right corner of the metal pipe.",
+                shortDesc: 'This lineup can catch enemies off guard by positioning on Cat',
+                video: '64HJGx82uCY',
+                standImage: 'https://i.imgur.com/vK5PFWw.png',
+                aimImage: 'https://i.imgur.com/orzpd1D.png'
+            },
+            {
+                id: 3,
+                title: 'B SITE FROM OUTSIDE MAIN',
+                desc: 'Position yourself at the corner of the wooden boxes on the ground. Position the left edge of the health hud line on the small black dot of the wooden roof. This lineup may seem difficult to learn but the spot is easily remembered once you use it.',
+                shortDesc: 'This lineup is very effective by providing a lot of protection and room for Viper to retake site',
+                video: '3vs9ojoC5o0',
+                standImage: 'https://i.imgur.com/EiCwXbU.png',
+                aimImage: 'https://i.imgur.com/uMRRKgt.png'
+            },
+            {
+                id: 4,
+                title: 'B SITE FROM PIZZA',
+                desc: "Position yourself at the corner of Pizza and aim at where the top-left and bottom-right corners of the metal roof meet.",
+                shortDesc: 'This lineup allows Viper to play away from common spots and hold site from Pizza',
+                video: 'PBxjAyqNhOY',
+                standImage: 'https://i.imgur.com/r4lL6sT.png',
+                aimImage: 'https://i.imgur.com/81BRWAM.png'
             },
         ]
     }
@@ -219,6 +263,8 @@ const MapPage: React.FunctionComponent<IPage> = props => {
             setMapInfo(haven)
         } else if (window.location.pathname === '/split') {
             setMapInfo(split)
+        } else if (window.location.pathname === '/ascent') {
+            setMapInfo(ascent)
         }
     }, [mapInfo])
 
